e6a73fb8696342e1c2caf1887bf653420477e283
Leonardo Alves Lopes <ra51781067848@cursos.insf>
2024-06-18 08:13:25 -0300

    Projeto alterado

--- exibicao.js
+++ exibicao.js
@@ -30,23 +30,23 @@ export function exibicao(saldo) {
 
 export function capital() {
   console.log("Qual será o capital")
-  capital = Number(ler())
+  let c = Number(ler())
 
-  return capital
+  return c
 }
 
 export function juros() {
   console.log("Qual será o juros")
-  juros = Number(ler())
+  let j = Number(ler())
 
-  return juros
+  return j
 }
 
 export function tempo() {
   console.log("Qual será o tempo")
-  tempo = Number(ler())
+  let t = Number(ler())
 
-  return tempo
+  return t
 }
 
 export function resultadoLucro (r){
